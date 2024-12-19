19b2ec52bfa3c8465bf136e2a0f9aeba64d27377
unknown <eddahaniouissal@gmail.com>
2024-12-19 19:33:12 +0100

    Commit initial

---
+++ src/main/java/com/control/tdd/fizzbuzz.java
@@ -0,0 +1,11 @@
+package com.control.tdd;
+
+public class fizzbuzz {
+
+
+    public String action(int input ) {
+        return "2";
+    }
+
+}
+
---
+++ src/test/java/com/control/tdd/fizzbuzzTest.java
@@ -0,0 +1,28 @@
+package com.control.tdd;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class fizzbuzzTest {
+
+
+
+    @Test
+    public void test() {
+        fizzbuzz fizzbuzz = new fizzbuzz();
+        Assertions.assertEquals("1", fizzbuzz.action(1));
+    }
+        @Test
+                public void test2() {
+            fizzbuzz fizzbuzz = new fizzbuzz();
+            Assertions.assertEquals("2",fizzbuzz.action(2));
+
+        }
+
+
+
+
+    }
+
+
+
